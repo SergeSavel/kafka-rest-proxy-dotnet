574b4b2ac5033404db015b06b8c4e961022fc047
Serge Savel <serge@savel.pro>
2021-02-10 23:35:29 +0400

    Admin controller added

---
+++ src/KafkaRestProxy/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pro.savel.KafkaRestProxy.Services;
+
+namespace pro.savel.KafkaRestProxy.Controllers
+{
+    [ApiController]
+    [Route("admin")]
+    public class AdminController : ControllerBase
+    {
+        private readonly AdminClientService _adminClientService;
+
+        public AdminController(AdminClientService adminClientService)
+        {
+            _adminClientService = adminClientService;
+        }
+
+        [HttpGet("{topic}")]
+        public TopicInfo GetTopicInfo(string topic)
+        {
+            return _adminClientService.GetTopicInfo(topic);
+        }
+    }
+}
