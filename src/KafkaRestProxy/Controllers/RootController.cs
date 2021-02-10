838b6c791a0440a990d74dc5d3ae19ece21d8a91
Serge Savel <serge@savel.pro>
2021-02-10 23:35:18 +0400

    Root controller added

---
+++ src/KafkaRestProxy/Controllers/RootController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace pro.savel.KafkaRestProxy.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class RootController : ControllerBase
+    {
+        [HttpGet]
+        public string GetVersion()
+        {
+            return "0.0.1";
+        }
+    }
+}
