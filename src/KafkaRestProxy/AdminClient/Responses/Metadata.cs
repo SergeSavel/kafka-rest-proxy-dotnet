d21d4141c1f2eceb2938ba6a9247dbb5481b0d7c
Serge Savel <serge@savel.pro>
2021-02-27 16:04:37 +0400

    Polishing

--- src/KafkaRestProxy/AdminClient/Responses/Metadata.cs
+++ src/KafkaRestProxy/AdminClient/Responses/Metadata.cs
@@ -6,11 +6,8 @@ namespace pro.savel.KafkaRestProxy.AdminClient.Responses
     public class Metadata
     {
         [Required] public IEnumerable<BrokerMetadata> Brokers { get; init; }
-
         [Required] public IEnumerable<TopicMetadata> Topics { get; init; }
-
         [Required] public int OriginatingBrokerId { get; init; }
-
         [Required] public string OriginatingBrokerName { get; init; }
     }
 }
--- src/KafkaRestProxy/AdminClient/Responses/TopicMetadata.cs
+++ src/KafkaRestProxy/AdminClient/Responses/TopicMetadata.cs
@@ -6,17 +6,13 @@ namespace pro.savel.KafkaRestProxy.AdminClient.Responses
     public class TopicMetadata
     {
         [Required] public string Name { get; init; }
-
         [Required] public IEnumerable<PartitionMetadata> Partitions { get; init; }
 
         public class PartitionMetadata
         {
             [Required] public int Id { get; init; }
-
             [Required] public int Leader { get; init; }
-
             [Required] public IEnumerable<int> Replicas { get; init; }
-
             [Required] public IEnumerable<int> InSyncReplicas { get; init; }
         }
     }
