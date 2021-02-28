79323790fa38d2b8dd0e06ea4df04d4b11f2de23
Serge Savel <serge@savel.pro>
2021-03-01 00:51:30 +0400

    Polishing

--- src/KafkaRestProxy/AdminClient/Requests/CreateTopicRequest.cs
+++ src/KafkaRestProxy/AdminClient/Requests/CreateTopicRequest.cs
@@ -5,7 +5,7 @@ namespace pro.savel.KafkaRestProxy.AdminClient.Requests
     public class CreateTopicRequest
     {
         [Required] public string Name { get; init; }
-        public int? NumPartitions { get; init; }
+        [Range(1, 100000)] public int? NumPartitions { get; init; }
         public short? ReplicationFactor { get; init; }
     }
 }
