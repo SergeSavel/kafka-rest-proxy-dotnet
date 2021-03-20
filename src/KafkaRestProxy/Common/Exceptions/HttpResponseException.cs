f8147164a5c3eb96e1835c2ee9bfde83bcf0a284
Serge Savel <serge@savel.pro>
2021-03-21 00:51:27 +0400

    Polishing

--- src/KafkaRestProxy/Common/Exceptions/HttpResponseException.cs
+++ src/KafkaRestProxy/Common/Exceptions/HttpResponseException.cs
@@ -4,17 +4,17 @@ namespace pro.savel.KafkaRestProxy.Common.Exceptions
 {
     public class HttpResponseException : Exception
     {
-        public HttpResponseException(string? message, Exception? innerException) : base(message, innerException)
+        public HttpResponseException(string message, Exception innerException) : base(message, innerException)
         {
             Value = message;
         }
 
-        public HttpResponseException(string? message) : base(message)
+        public HttpResponseException(string message) : base(message)
         {
             Value = message;
         }
 
-        public HttpResponseException(Exception? innerException) : base(null, innerException)
+        public HttpResponseException(Exception innerException) : base(null, innerException)
         {
         }
 
