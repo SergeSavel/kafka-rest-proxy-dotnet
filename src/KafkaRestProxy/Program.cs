38d5730629c5e3656c00d695ec2d92b129d61ed2
Serge Savel <serge@savel.pro>
2021-04-01 16:28:43 +0400

    Default port changed

--- src/KafkaRestProxy/Program.cs
+++ src/KafkaRestProxy/Program.cs
@@ -13,7 +13,12 @@ namespace SergeSavel.KafkaRestProxy
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                        .UseStartup<Startup>()
+                        .UseUrls("http://localhost:8086");
+                })
                 .UseWindowsService();
         }
     }
