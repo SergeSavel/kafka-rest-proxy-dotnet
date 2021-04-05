6aada00c666bb302ac761ba9f124f4f48a50fd1a
Serge Savel <serge@savel.pro>
2021-04-06 00:24:20 +0400

    Polishing

--- src/KafkaRestProxy/Startup.cs
+++ src/KafkaRestProxy/Startup.cs
@@ -62,8 +62,6 @@ namespace SergeSavel.KafkaRestProxy
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "KafkaRestProxy v1"));
             }
 
-            //app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthentication();
