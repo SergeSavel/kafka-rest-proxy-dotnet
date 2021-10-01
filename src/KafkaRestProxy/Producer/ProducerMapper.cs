982cb41882484317bf7d44126e12cdf4e7a30ce2
Serge Savel <serge@savel.pro>
2021-10-01 17:31:36 +0400

    Obsolete methods removed

--- src/KafkaRestProxy/Producer/ProducerMapper.cs
+++ src/KafkaRestProxy/Producer/ProducerMapper.cs
@@ -13,95 +13,15 @@
 // limitations under the License.
 
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Text;
-using Avro;
-using Avro.Generic;
 using Confluent.Kafka;
-using SergeSavel.KafkaRestProxy.Common.Exceptions;
 using SergeSavel.KafkaRestProxy.Producer.Contract;
-using SergeSavel.KafkaRestProxy.Producer.Requests;
 
 namespace SergeSavel.KafkaRestProxy.Producer
 {
     public static class ProducerMapper
     {
-        [Obsolete]
-        public static Message<string, string> MapMessageStringString(PostMessageRequest source)
-        {
-            var result = new Message<string, string>
-            {
-                Key = source.Key,
-                Value = source.Value
-            };
-
-            if (source.Headers != null) result.Headers = MapHeaders(source.Headers);
-
-            return result;
-        }
-
-        [Obsolete]
-        public static Message<string, GenericRecord> MapMessageStringGeneric(PostMessageRequest source,
-            ConcurrentDictionary<string, RecordSchema> schemaCache)
-        {
-            GenericRecord valueRecord;
-
-            try
-            {
-                valueRecord = ProducerGenericMapper.GetGenericRecord(source.Value, source.ValueSchema, schemaCache);
-            }
-            catch (InvalidOperationException e)
-            {
-                throw new BadRequestException(e.Message);
-            }
-
-            var result = new Message<string, GenericRecord>
-            {
-                Key = source.Key,
-                Value = valueRecord
-            };
-
-            if (source.Headers != null) result.Headers = MapHeaders(source.Headers);
-
-            return result;
-        }
-
-        [Obsolete]
-        public static Message<GenericRecord, GenericRecord> MapMessageGenericGeneric(PostMessageRequest source,
-            ConcurrentDictionary<string, RecordSchema> schemaCache)
-        {
-            var keyRecord = ProducerGenericMapper.GetGenericRecord(source.Key, source.KeySchema, schemaCache);
-            var valueRecord = ProducerGenericMapper.GetGenericRecord(source.Value, source.ValueSchema, schemaCache);
-
-            var result = new Message<GenericRecord, GenericRecord>
-            {
-                Key = keyRecord,
-                Value = valueRecord
-            };
-
-            if (source.Headers != null) result.Headers = MapHeaders(source.Headers);
-
-            return result;
-        }
-
-        [Obsolete]
-        public static Message<GenericRecord, string> MapMessageGenericString(PostMessageRequest source,
-            ConcurrentDictionary<string, RecordSchema> schemaCache)
-        {
-            var keyRecord = ProducerGenericMapper.GetGenericRecord(source.Key, source.KeySchema, schemaCache);
-
-            var result = new Message<GenericRecord, string>
-            {
-                Key = keyRecord,
-                Value = source.Value
-            };
-
-            if (source.Headers != null) result.Headers = MapHeaders(source.Headers);
-
-            return result;
-        }
-
         public static Headers MapHeaders(IEnumerable<KeyValuePair<string, string>> source)
         {
             var result = new Headers();
