e0117fc970e18ee71674bb153c163a9498c76f8e
Serge Savel <serge@savel.pro>
2022-02-22 16:41:20 +0400

    Delete unused files

--- publish-win-x64.cmd
+++
@@ -1,3 +0,0 @@
-del ..\publish-win-x64\* /F /Q
-dotnet clean --configuration Release --runtime win-x64
-dotnet publish --configuration Release --runtime win-x64 --self-contained --output ..\publish-win-x64
--- publish-win-x86.cmd
+++
@@ -1,3 +0,0 @@
-del ..\publish-win-x86\* /F /Q
-dotnet clean --configuration Release --runtime win-x86
-dotnet publish --configuration Release --runtime win-x86 --self-contained --output ..\publish-win-x86
