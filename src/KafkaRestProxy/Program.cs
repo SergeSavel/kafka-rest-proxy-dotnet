416f786928f78eddc773ffde6eac2be28d16c15e
Serge Savel <serge@savel.pro>
2024-09-19 11:46:04 +0400

    Copyright update

--- src/KafkaRestProxy/Program.cs
+++ src/KafkaRestProxy/Program.cs
@@ -1,4 +1,4 @@
-// Copyright 2023 Sergey Savelev
+// Copyright 2024 Sergey Savelev
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
